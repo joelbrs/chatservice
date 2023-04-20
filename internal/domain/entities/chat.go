3cf1c1956b39ef96bcc495cc8edf0f8c7598d976
Joel Filho <joelfilho02812@gmail.com>
2023-04-20 17:41:22 -0300

    add chat methods

--- internal/domain/entities/chat.go
+++ internal/domain/entities/chat.go
@@ -1,6 +1,10 @@
 package entities
 
-import "errors"
+import (
+	"errors"
+
+	"github.com/google/uuid"
+)
 
 type ChatConfig struct {
 	Model            *Model
@@ -24,6 +28,42 @@ type Chat struct {
 	Config               *ChatConfig
 }
 
+func NewChat(userID string, initialSystemMessage *Message, chatConfig *ChatConfig) (*Chat, error) {
+	chat := &Chat{
+		ID:                   uuid.New().String(),
+		UserID:               userID,
+		InitialSystemMessage: initialSystemMessage,
+		Status:               "active",
+		Config:               chatConfig,
+		TokenUsage:           0,
+	}
+
+	chat.AddMessage(initialSystemMessage)
+
+	if err := chat.Validate(); err != nil {
+		return nil, err
+	}
+
+	return chat, nil
+}
+
+func (c *Chat) Validate() error {
+
+	if c.UserID == "" {
+		return errors.New("user id is empty")
+	}
+
+	if c.Status != "active" && c.Status != "ended" {
+		return errors.New("invalid status")
+	}
+
+	if c.Config.Temperature < 0 || c.Config.Temperature > 2 {
+		return errors.New("invalid temperature")
+	}
+
+	return nil
+}
+
 func (c *Chat) AddMessage(m *Message) error {
 	if c.Status == "ended" {
 		return errors.New("chat is ended. no more messages allowed")
@@ -35,9 +75,24 @@ func (c *Chat) AddMessage(m *Message) error {
 			c.RefreshTokenUsage()
 			break
 		}
+		c.ErasedMessages = append(c.ErasedMessages, c.Messages[0])
+		c.Messages = c.Messages[1:]
+		c.RefreshTokenUsage()
 	}
 
 	return nil
+}
+
+func (c *Chat) GetMessages() []*Message {
+	return c.Messages
+}
+
+func (c *Chat) CountMessages() int {
+	return len(c.Messages)
+}
+
+func (c *Chat) End() {
+	c.Status = "ended"
 }
 
 func (c *Chat) RefreshTokenUsage() {
