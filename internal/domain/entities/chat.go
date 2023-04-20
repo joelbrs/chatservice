384cc2d46fb2bed0459d341464a56c5fa399b7dc
Joel Filho <joelfilho02812@gmail.com>
2023-04-20 13:00:56 -0300

    first commit

---
+++ internal/domain/entities/chat.go
@@ -0,0 +1,49 @@
+package entities
+
+import "errors"
+
+type ChatConfig struct {
+	Model            *Model
+	Temperature      float32
+	TopP             float32
+	N                int
+	Stop             []string
+	MaxTokens        int
+	PresencePenalty  float32
+	FrequencyPenalty float32
+}
+
+type Chat struct {
+	ID                   string
+	UserID               string
+	InitialSystemMessage *Message
+	Messages             []*Message
+	ErasedMessages       []*Message
+	Status               string
+	TokenUsage           int
+	Config               *ChatConfig
+}
+
+func (c *Chat) AddMessage(m *Message) error {
+	if c.Status == "ended" {
+		return errors.New("chat is ended. no more messages allowed")
+	}
+
+	for {
+		if c.Config.Model.GetMaxTokens() >= m.GetQtdTokens()+c.TokenUsage {
+			c.Messages = append(c.Messages, m)
+			c.RefreshTokenUsage()
+			break
+		}
+	}
+
+	return nil
+}
+
+func (c *Chat) RefreshTokenUsage() {
+	c.TokenUsage = 0
+
+	for m := range c.Messages {
+		c.TokenUsage += c.Messages[m].GetQtdTokens()
+	}
+}
